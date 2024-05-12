1fa7bc13e0ae4c8322f1d8059e3a857e3de390eb
Tannaz M <tannaz.monajemi@gmail.com>
2024-05-12 12:07:57 +0330

    Enhance RDF export functionality with comprehensive data representation
    - Enhanced the `dataframe_to_ttl` function in the RDF export script
    `utils/export_to_rdf.py` to dynamically include all specified columns from
    the DataFrame, ensuring a more comprehensive data representation in the
    RDF format.
    
    Key Changes:
    - Updated the `REQUIRED_COLUMNS` list to include all recent DataFrame
    columns: `duplicate_flag`, `null_value_flag`, `domain_extraction_flag`,
    `incomplete_url_flag`, `base_repo_url_flag`, and `explanation`.
    - Implemented a dynamic approach for RDF triple creation, using column
    names directly from the `REQUIRED_COLUMNS` list to generate RDF
    predicates.
    - Added error handling to skip rows with processing errors and log these
    incidents
    
    - This is related to the issue #67 and #11

--- utils/export_to_rdf.py
+++ utils/export_to_rdf.py
@@ -1,5 +1,5 @@
-from rdflib import Graph, Literal, URIRef, RDF, Namespace
-from rdflib.namespace import FOAF, XSD, RDFS
+from rdflib import Graph, Literal, URIRef, Namespace
+from rdflib.namespace import XSD
 from loguru import logger
 
 # List of required columns
@@ -7,11 +7,17 @@ REQUIRED_COLUMNS = [
     "projectref",
     "nlnetpage",
     "repourl",
+    "duplicate_flag",
+    "null_value_flag",
     "repodomain",
+    "domain_extraction_flag",
+    "incomplete_url_flag",
     "base_repo_url",
+    "base_repo_url_flag",
     "testfilecountlocal",
     "clone_status",
     "last_commit_hash",
+    "explanation",
 ]
 
 
@@ -39,65 +45,29 @@ def dataframe_to_ttl(df):
     for index, row in df.iterrows():
         try:
             graph = Graph()
+            subject_uri = URIRef(base_uri + str(row.get("projectref")))
 
-            # Extracting column values using `.get` for safety
-            project_ref = row.get("projectref")
-            nlnet_page = row.get("nlnetpage")
-            repo_url = row.get("repourl")
-            repo_domain = row.get("repodomain")
-            base_repo_url = row.get("base_repo_url")
-            test_file_count = row.get("testfilecountlocal")
-            clone_status = row.get("clone_status")
-            last_commit_hash = row.get("last_commit_hash")
+            # Create triples for all fields
+            for column in REQUIRED_COLUMNS:
+                value = row.get(column)
+                if value is not None and value != -1:  # Check for valid values,
+                    # ignoring placeholder like -1
+                    predicate = project_namespace[
+                        column
+                    ]  # Using column name as predicate
+                    if isinstance(value, str) and value.startswith("http"):
+                        graph.add(
+                            (subject_uri, predicate, URIRef(value))
+                        )  # Add as URIRef for URLs
+                    else:
+                        graph.add(
+                            (
+                                subject_uri,
+                                predicate,
+                                Literal(value, datatype=XSD.string),
+                            )
+                        )
 
-            subject_uri = URIRef(base_uri + project_ref)
-
-            # Basic triples
-            graph.add((subject_uri, RDF.type, FOAF.Project))
-            if nlnet_page:
-                graph.add((subject_uri, FOAF.homepage, URIRef(nlnet_page)))
-            if repo_url:
-                graph.add((subject_uri, RDFS.seeAlso, URIRef(repo_url)))
-
-            # Additional triples
-            if repo_domain:
-                graph.add(
-                    (
-                        subject_uri,
-                        project_namespace.repoDomain,
-                        Literal(repo_domain, datatype=XSD.string),
-                    )
-                )
-            if base_repo_url:
-                graph.add(
-                    (subject_uri, project_namespace.baseRepoURL, URIRef(base_repo_url))
-                )
-            if test_file_count is not None:
-                graph.add(
-                    (
-                        subject_uri,
-                        project_namespace.testFileCount,
-                        Literal(test_file_count, datatype=XSD.integer),
-                    )
-                )
-            if clone_status:
-                graph.add(
-                    (
-                        subject_uri,
-                        project_namespace.cloneStatus,
-                        Literal(clone_status, datatype=XSD.string),
-                    )
-                )
-            if last_commit_hash:
-                graph.add(
-                    (
-                        subject_uri,
-                        project_namespace.lastCommitHash,
-                        Literal(last_commit_hash, datatype=XSD.string),
-                    )
-                )
-
-            # Serialise the RDF graph to a string in TTL format and add to list
             ttl_strings.append(graph.serialize(format="turtle"))
 
         except Exception as e:
