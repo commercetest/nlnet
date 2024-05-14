f97841dd1ab4beb38d2bb12a608f860ae2949280
Tannaz M <tannaz.monajemi@gmail.com>
2024-05-14 17:41:18 +0330

    Enhance Sankey Diagram with Multi-layered Node Integration
    This commit significantly enhances the Sankey diagram by introducing
    multiple new layers and refining node interactions based on the project's
    requirements. Key improvements include:
    
    1. Addition of test runner nodes linked exclusively from the 'Repos
    Cloned' node to accurately depict test environment utilisation.
    2. Integration of 'Base Repo URL Issues' into the third layer, ensuring
    comprehensive visibility of repository health alongside 'Duplicates' and
    'Incomplete URLs'.
    3. Implementation of dynamic node creation for test file counts and test
    runners, facilitating a detailed analysis of testing data.
    4. Refinement of data linkage logic to exclude duplicate entries from
    affecting subsequent analysis stages, ensuring data integrity across
    visualisations.
    
    This is related to the issue #56 #69
    
    your changes. Lines
    starting

--- src/sankey_diagram_plotly.py
+++ src/sankey_diagram_plotly.py
@@ -1,145 +1,285 @@
 """
-Script for processing domain-specific data and generating a Sankey diagram of
-the analysis.
+This script performs analysis on a dataset containing information about various
+code hosting platform repositories and visualises the results using a Sankey
+diagram.
 
-This script reads an original data file, processes domain-specific data, and
-visualises the analysis results using a Sankey diagram. The processing involves
-the following steps:
-1. Removing duplicates from domain-specific data.
-2. Filtering out entries with incomplete URLs.
-3. Dropping rows with null values.
 
-The script sets up the working directory and logger, reads the original data
-file, and filters the data to include only domains with ten or more
-occurrences. It then calculates various metrics for each domain, including the
- number of duplicates, incomplete URLs, and null values.
-
-Based on the processed data, the script constructs a Sankey diagram to
-visualise the flow of data analysis, including transitions from the original
-data to domain-specific data, and from domains to outcome nodes
-representing duplicates, entries with incomplete URLs, and rows with null values.
-
-The generated Sankey diagram is saved as an HTML file in the
-'reports/graphs/sankey-diagram-of-analysis' directory.
+Variables:
+    - working_directory: Path to the working directory or Git root directory.
+    - df: DataFrame containing the original data read from a CSV file.
+    - test_file_categories: Dictionary categorising test file counts into bins.
+    - domain_counts: Series containing the count of repositories per domain.
+    - domains_more_than_ten: List of domains with more than ten repositories.
+    - domains_less_than_ten_count: Total count of repositories across domains
+    with ten or fewer repositories.
+    - node_dict: Dictionary mapping node names to numerical indices for Sankey
+    diagram visualisation.
+    - sources, targets, values: Lists storing sources, targets, and values for
+    Sankey diagram links.
 """
 
 import pandas as pd
-import plotly.graph_objects as go
 from loguru import logger
-
 from utils.git_utils import get_working_directory_or_git_root
-from utils.initial_data_preparation import (
-    filter_out_incomplete_urls,
-    remove_duplicates,
-)
+import plotly.graph_objects as go
+import re
+from collections import defaultdict
 
 
-def process_domain_df(domain_df):
-    # Removing duplicates
-    unique_df, num_duplicates = remove_duplicates(domain_df)
+def add_node(node_name):
+    global counter
+    if node_name not in node_dict:
+        node_dict[node_name] = counter
+        counter += 1
 
-    # Filtering out incomplete URLs
-    complete_df = filter_out_incomplete_urls(unique_df)
-    num_incomplete_urls = len(unique_df) - len(complete_df)
 
-    # Dropping null values
-    no_null_df = complete_df.dropna()
-    num_null_values = len(complete_df) - len(no_null_df)
+# Function to parse the test_runners column and return a dict of test runners
+# and their counts
+def parse_test_runners(runners_str):
+    """Parses the 'test_runners' column in the dataset to extract test runners
+    and their counts."""
 
-    return {
-        "duplicates_count": num_duplicates,
-        "incomplete_urls_count": num_incomplete_urls,
-        "null_values_count": num_null_values,
-    }
+    if pd.isna(runners_str) or runners_str in ["nan", "None detected"]:
+        return {}
+    runners = defaultdict(int)
+    parts = runners_str.split(",")
+    for part in parts:
+        match = re.match(r"(\w+):\s*(\d+)", part.strip())
+        if match:
+            runners[match.group(1)] += int(match.group(2))
+    return dict(runners)
 
 
 # Setting up the working directory and logger
 working_directory = get_working_directory_or_git_root()
 logger.info(f"Working directory is: {working_directory}")
 
-
 # Reading the original data
-original_df = pd.read_csv(working_directory / "data" / "original.csv")
-initial_row_count = original_df.shape[0]
+df = pd.read_csv(working_directory / "data" / "updated_local_github_df_test_count.csv")
 
-original_df["repodomain"] = original_df["repourl"].str.extract(
-    r"https?://(www\.)?([^/]+)"
-)[1]
-repodomain_counts = original_df["repodomain"].value_counts()
+# Clone status updated for null values as 'not cloned'
+df["clone_status"] = df["clone_status"].fillna("not cloned")
+df["Repos Not Cloned"] = df["clone_status"] == "not cloned"
+df["Repos Cloned"] = df["clone_status"] == "successful"
 
-domains_with_more_than_ten = repodomain_counts[repodomain_counts >= 10].index
+# Apply the parsing function to the test_runners column
+df["parsed_test_runners"] = df["test_runners"].apply(parse_test_runners)
 
+# Sum test runners for cloned repositories
+test_runner_counts = defaultdict(int)
+for index, row in df[df["Repos Cloned"]].iterrows():
+    for runner, count in row["parsed_test_runners"].items():
+        test_runner_counts[runner] += count
 
-# Filter the original DataFrame
-filtered_original_df = original_df[
-    original_df["repodomain"].isin(domains_with_more_than_ten)
+# Test file counts categorized
+bins = [-1, 0, 9, 99, 999, float("inf")]
+labels = [
+    "0 test files",
+    "1 - 9 test files",
+    "10 - 99 test files",
+    "100 - 999 test files",
+    "More than 1000 test files",
 ]
-repodomain_counts = filtered_original_df["repodomain"].value_counts()
+df["Test File Categories"] = pd.cut(
+    df["testfilecountlocal"], bins=bins, labels=labels, right=True
+)
 
-# Nodes for Sankey Diagram
-node_labels = [
-    # 0
-    "Original Data",
-    # 1-3
-    *repodomain_counts.index.tolist(),
-    # 4
-    "Duplicates",
-    # 5
-    "No Repo Name",
-    # 6
-    "Null values",
-]
+# Categories for test files
+test_file_categories = {
+    "0 test files": (0, 0),
+    "1 - 9 test files": (1, 9),
+    "10 - 99 test files": (10, 99),
+    "100 - 999 test files": (100, 999),
+    "More than 1000 test files": (1000, float("inf")),  # Renamed for clarity
+}
 
-# Links for Sankey Diagram
-# Initialise the indices for sources and targets
-source_indices = []
-target_indices = []
+# Prepare domain counts
+domain_counts = df["repodomain"].value_counts()
+domains_more_than_ten = domain_counts[domain_counts > 10].index.tolist()
+domains_less_than_ten_count = domain_counts[domain_counts <= 10].sum()
+
+# Node dictionary for indexing
+node_dict = {}
+counter = 0
+
+# Initialise source, target, and value lists for Sankey
+sources = []
+targets = []
 values = []
 
-# Original Data to domains
-for i, domain in enumerate(repodomain_counts.index, start=1):
-    source_indices.append(0)
-    target_indices.append(i)
-    values.append(repodomain_counts[domain])
+# Add nodes
+add_node("Original Data")
+for domain in domains_more_than_ten:
+    add_node(domain)
+add_node("Domains with < 10 Repos")
+add_node("Duplicates")
+add_node("Incomplete URLs")
+add_node("Base Repo URL Issues")
+add_node("Repos not Cloned")
+add_node("Repos Cloned")
 
 
-# Calculate domain results and aggregate to outcome nodes
-domain_results = {}
-duplicates_total = 0
-incomplete_urls_total = 0
-null_values_total = 0
+for category in test_file_categories.keys():
+    add_node(category)
 
-for domain in repodomain_counts.index:
-    domain_df = filtered_original_df[filtered_original_df["repodomain"] == domain]
-    result = process_domain_df(domain_df)
-    domain_results[domain] = result
-    duplicates_total += result["duplicates_count"]
-    incomplete_urls_total += result["incomplete_urls_count"]
-    null_values_total += result["null_values_count"]
+# Add nodes and links for test runners
+for runner, count in test_runner_counts.items():
+    add_node(runner)  # Create a node for each test runner
 
-# Adding links from each domain to the outcome nodes
-duplicate_index = len(repodomain_counts) + 1
-incomplete_url_index = duplicate_index + 1
-null_value_index = incomplete_url_index + 1
+# Link from 'Original Data' to individual domains and grouped node
+sources.append(node_dict["Original Data"])
+targets.append(node_dict["Domains with < 10 Repos"])
+values.append(domains_less_than_ten_count)
 
-for i, domain in enumerate(repodomain_counts.index, start=1):
-    # Link to Duplicates
-    source_indices.append(i)
-    target_indices.append(duplicate_index)
-    values.append(domain_results[domain]["duplicates_count"])
+for domain in domains_more_than_ten:
+    sources.append(node_dict["Original Data"])
+    targets.append(node_dict[domain])
+    values.append(domain_counts[domain])
 
-    # Link to Incomplete URLs
-    source_indices.append(i)
-    target_indices.append(incomplete_url_index)
-    values.append(domain_results[domain]["incomplete_urls_count"])
+    # Link domains to Duplicates if applicable
+    domain_duplicates_count = df[
+        (df["repodomain"] == domain) & df["duplicate_flag"]
+    ].shape[0]
+    if domain_duplicates_count > 0:
+        sources.append(node_dict[domain])
+        targets.append(node_dict["Duplicates"])
+        values.append(domain_duplicates_count)
 
-    # Link to Null Values
-    source_indices.append(i)
-    target_indices.append(null_value_index)
-    values.append(domain_results[domain]["null_values_count"])
+    # Link domain to Incomplete URLs if applicable, excluding duplicates
+    domain_incomplete_urls_count = df[
+        (df["repodomain"] == domain) & df["incomplete_url_flag"] & ~df["duplicate_flag"]
+    ].shape[0]
+    if domain_incomplete_urls_count > 0:
+        sources.append(node_dict[domain])
+        targets.append(node_dict["Incomplete URLs"])
+        values.append(domain_incomplete_urls_count)
+
+    # Link domain to Base Repo URL Issues if applicable, excluding duplicates
+    # and Incomplete URLs
+    domain_base_repo_url_issues_count = df[
+        (df["repodomain"] == domain)
+        & df["base_repo_url_flag"]
+        & ~df["incomplete_url_flag"]
+        & ~df["duplicate_flag"]
+    ].shape[0]
+    if domain_base_repo_url_issues_count > 0:
+        sources.append(node_dict[domain])
+        targets.append(node_dict["Base Repo URL Issues"])
+        values.append(domain_base_repo_url_issues_count)
+
+    # Link domain to Repos Not Cloned if applicable, excluding duplicates,
+    # Incomplete URLs, Base Repo URL Issues
+    domain_repos_not_cloned_count = df[
+        (df["repodomain"] == domain)
+        & df["Repos Not Cloned"]
+        & ~df["base_repo_url_flag"]
+        & ~df["incomplete_url_flag"]
+        & ~df["duplicate_flag"]
+    ].shape[0]
+    if domain_repos_not_cloned_count > 0:
+        sources.append(node_dict[domain])
+        targets.append(node_dict["Repos not Cloned"])
+        values.append(domain_repos_not_cloned_count)
+
+    # Link domain to Repos Cloned if applicable, excluding duplicates,
+    # Incomplete URLs, Base Repo URL Issues
+    domain_repos_cloned_count = df[
+        (df["repodomain"] == domain)
+        & df["Repos Cloned"]
+        & ~df["base_repo_url_flag"]
+        & ~df["incomplete_url_flag"]
+        & ~df["duplicate_flag"]
+    ].shape[0]
+    if domain_repos_cloned_count > 0:
+        sources.append(node_dict[domain])
+        targets.append(node_dict["Repos Cloned"])
+        values.append(domain_repos_cloned_count)
+
+# Link 'Domains with < 10 Repos' to Duplicates if applicable
+less_than_ten_duplicates_count = df[
+    (df["repodomain"].isin(domain_counts[domain_counts <= 10].index))
+    & df["duplicate_flag"]
+].shape[0]
+if less_than_ten_duplicates_count > 0:
+    sources.append(node_dict["Domains with < 10 Repos"])
+    targets.append(node_dict["Duplicates"])
+    values.append(less_than_ten_duplicates_count)
+
+# Link 'Domains with < 10 Repos' to Incomplete URLs if applicable, excluding duplicates
+less_than_ten_incomplete_urls_count = df[
+    (df["repodomain"].isin(domain_counts[domain_counts <= 10].index))
+    & df["incomplete_url_flag"]
+    & ~df["duplicate_flag"]
+].shape[0]
+if less_than_ten_incomplete_urls_count > 0:
+    sources.append(node_dict["Domains with < 10 Repos"])
+    targets.append(node_dict["Incomplete URLs"])
+    values.append(less_than_ten_incomplete_urls_count)
+
+# Link 'Domains with < 10 Repos' to Base Repo URL Issues if applicable, excluding duplicates and Incomplete URLs
+less_than_ten_base_repo_url_issues_count = df[
+    (df["repodomain"].isin(domain_counts[domain_counts <= 10].index))
+    & df["base_repo_url_flag"]
+    & ~df["incomplete_url_flag"]
+    & ~df["duplicate_flag"]
+].shape[0]
+if less_than_ten_base_repo_url_issues_count > 0:
+    sources.append(node_dict["Domains with < 10 Repos"])
+    targets.append(node_dict["Base Repo URL Issues"])
+    values.append(less_than_ten_base_repo_url_issues_count)
+
+# Link 'Domains with < 10 Repos' to Repos Not Cloned if applicable, excluding
+# duplicates, Incomplete URLs, Base Repo URL Issues
+less_than_ten_repos_not_cloned_count = df[
+    (df["repodomain"].isin(domain_counts[domain_counts <= 10].index))
+    & df["Repos Not Cloned"]
+    & ~df["base_repo_url_flag"]
+    & ~df["incomplete_url_flag"]
+    & ~df["duplicate_flag"]
+].shape[0]
+if less_than_ten_repos_not_cloned_count > 0:
+    sources.append(node_dict["Domains with < 10 Repos"])
+    targets.append(node_dict["Repos not Cloned"])
+    values.append(less_than_ten_repos_not_cloned_count)
+
+# Link 'Domains with < 10 Repos' to Repos Cloned if applicable, excluding
+# duplicates, Incomplete URLs, Base Repo URL Issues
+less_than_ten_repos_cloned_count = df[
+    (df["repodomain"].isin(domain_counts[domain_counts <= 10].index))
+    & df["Repos Cloned"]
+    & ~df["base_repo_url_flag"]
+    & ~df["incomplete_url_flag"]
+    & ~df["duplicate_flag"]
+].shape[0]
+if less_than_ten_repos_cloned_count > 0:
+    sources.append(node_dict["Domains with < 10 Repos"])
+    targets.append(node_dict["Repos Cloned"])
+    values.append(less_than_ten_repos_cloned_count)
 
 
-# Creating the Sankey Diagram
+# Link from 'Repos Cloned' to the test file categories
+repos_cloned_index = node_dict["Repos Cloned"]
+for category, (lower_bound, upper_bound) in test_file_categories.items():
+    # Filter df to find counts within bounds for cloned repos
+    count = df[
+        (df["Repos Cloned"])
+        & (df["testfilecountlocal"] >= lower_bound)
+        & (df["testfilecountlocal"] <= upper_bound)
+    ].shape[0]
+    if count > 0:
+        sources.append(repos_cloned_index)
+        targets.append(node_dict[category])
+        values.append(count)
+
+# Link from 'Repos Cloned' to the test runners
+for runner, count in test_runner_counts.items():
+    if count > 0:
+        sources.append(repos_cloned_index)
+        targets.append(node_dict[runner])
+        values.append(count)
+
+
+# Visualise the Sankey Diagram including the Duplicates layer
 fig = go.Figure(
     data=[
         go.Sankey(
@@ -147,26 +287,14 @@ fig = go.Figure(
                 pad=15,
                 thickness=20,
                 line=dict(color="black", width=0.5),
-                label=node_labels,
+                label=[k for k in node_dict.keys()],
             ),
-            link=dict(
-                source=source_indices,
-                target=target_indices,
-                value=values,
-                # Adding labels for each link indicating the number of excluded rows
-                label=[f"{v} rows excluded" for v in values],
-            ),
+            link=dict(source=sources, target=targets, value=values),
         )
     ]
 )
-
-
-fig.write_html(
-    working_directory
-    / "reports"
-    / "graphs"
-    / "sankey-diagram-of-analysis"
-    / "sankey-diagram-plotly.html"
+fig.update_layout(
+    title_text="Extended Project Analysis Sankey Diagram with " "Test Runners",
+    font_size=12,
 )
-
 fig.show()
